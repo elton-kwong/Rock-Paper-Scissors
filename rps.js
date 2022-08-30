36d6779404f828614e28cb5547eca3396690bf66
elton-kwong <eltonjkwong@hotmail.com>
2022-08-30 01:53:39 -0400

    Modified Gameplay Settings
    Created functions to log 5 rounds, include spell-check and display score at the end

--- rps.js
+++ rps.js
@@ -11,41 +11,94 @@ function getComputerChoice() {
 
 // console.log(getComputerChoice(choices))
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock") {
-        if (computerSelection == "paper") {
-            return "You lose! Paper beats Rock!";
-        } else if (computerSelection == "scissors") {
-            return "You win! Rock beats Scissors!";
-        } else {
-            return "You tied! We both chose Rock!";
-        }
-    }
+const scoreboard = [];
 
-    else if (playerSelection == "paper") {
-        if (computerSelection == "scissors") {
-            return "You lose! Scissors beats Paper!";
-        } else if (computerSelection == "rock") {
-            return "You win! Paper beats Rock!";
-        } else {
-            return "You tied! We both chose Paper!";
-        }
-    }
-
-    else if (playerSelection == "scissors") {
-        if (computerSelection == "rock") {
-            return "You lose! Rock beats Scissors!";
-        } else if (computerSelection == "paper") {
-            return "You win! Scissors beats Paper!";
-        } else {
-            return "You tied! We both chose Scissors!";
-        }
-    }
+function spellCheck(options) {
+    return choices.includes(options);
 }
 
-const initialSelection = "ROCK";
-const playerSelection = initialSelection.toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(`You chose ${playerSelection}`);
-console.log(`Computer chose ${computerSelection}`);
-console.log(playRound(playerSelection, computerSelection));
+for (let i = 1; i <= 5; i++) {
+    console.log(`Round: ${i}`);
+    game();
+}
+
+function game() {
+    let initialSelection = prompt("Rock, Paper or Scissors?");
+    let playerSelection = initialSelection.toLowerCase();
+    let check = spellCheck(playerSelection);
+    while (check == false) {
+        playerSelection = prompt("Please choose a valid option. Make sure your spelling is correct!");
+        check = spellCheck(playerSelection);
+    }
+    const computerSelection = getComputerChoice();
+    console.log(`You chose ${playerSelection}`);
+    console.log(`Computer chose ${computerSelection}`);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("----------------------------------------------------------------------------------------------------");
+    const scores = playRound(playerSelection, computerSelection);
+    scoreboard.push(scores);
+}
+
+function playRound(playerSelection, computerSelection) {
+    let results; 
+
+    if (playerSelection === computerSelection) {
+        results = "Tie";
+    } else if (
+        playerSelection === "rock" && computerSelection === "paper" || 
+        playerSelection === "paper" && computerSelection === "scissors" || 
+        playerSelection === "scissors" && computerSelection === "rock") {
+        results = "Computer Wins";
+    } else {
+        results = "Player Wins";
+    }
+
+    return results;
+    // if (playerSelection == "rock") {
+    //     if (computerSelection == "paper") {
+    //         return "You lose! Paper beats Rock!";
+    //     } else if (computerSelection == "scissors") {
+    //         return "You win! Rock beats Scissors!";
+    //     } else {
+    //         return "You tied! We both chose Rock!";
+    //     }
+    // }
+
+    // else if (playerSelection == "paper") {
+    //     if (computerSelection == "scissors") {
+    //         return "You lose! Scissors beats Paper!";
+    //     } else if (computerSelection == "rock") {
+    //         return "You win! Paper beats Rock!";
+    //     } else {
+    //         return "You tied! We both chose Paper!";
+    //     }
+    // }
+
+    // else if (playerSelection == "scissors") {
+    //     if (computerSelection == "rock") {
+    //         return "You lose! Rock beats Scissors!";
+    //     } else if (computerSelection == "paper") {
+    //         return "You win! Scissors beats Paper!";
+    //     } else {
+    //         return "You tied! We both chose Scissors!";
+    //     }
+    // }
+}
+
+function getOccurrence(array, value) {
+    var count = 0;
+    array.forEach((v) => (v === value && count++));
+    return count;
+}
+
+console.log("Final Score:");
+console.log("Computer: ", getOccurrence(scoreboard, "Computer Wins"));
+console.log("Player: ", getOccurrence(scoreboard, "Player Wins"));
+console.log("Ties: ", getOccurrence(scoreboard, "Tie"));
+
+// const initialSelection = "ROCK";
+// const playerSelection = initialSelection.toLowerCase();
+// const computerSelection = getComputerChoice();
+// console.log(`You chose ${playerSelection}`);
+// console.log(`Computer chose ${computerSelection}`);
+// console.log(playRound(playerSelection, computerSelection));
